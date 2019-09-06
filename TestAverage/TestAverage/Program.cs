81295ffe1ced6568652edbc3475f78e95a31b303
Stefano D Minervini <minervini@mac.com>
2019-09-06 13:39:39 -0500

    Average score code written

--- TestAverage/TestAverage/Program.cs
+++ TestAverage/TestAverage/Program.cs
@@ -6,7 +6,24 @@ namespace TestAverage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hey you, let's get your average score?");
+            Console.WriteLine("First I'll need a few of your scores.");
+
+            Console.Write("What is your first score?  ");
+            var firstScore = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What is your second score?  ");
+            var secondScore = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What is your third score?  ");
+            var thirdScore = Convert.ToInt32(Console.ReadLine());
+
+            int averageScore = (firstScore + secondScore + thirdScore) / 3;
+
+            
+            Console.WriteLine($"{firstScore} {secondScore} {thirdScore}");
+
+            Console.WriteLine($"Your average score is: {averageScore}!");
         }
     }
 }
